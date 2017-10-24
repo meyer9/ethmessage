631cd28e58ebf0b4372b0da9d1f3a1ff059ac62e
Julian Meyer <julianmeyer2000@gmail.com>
2017-10-23 18:29:08 -0700

    Added max length of message

--- truffle.js
+++ truffle.js
@@ -1,9 +1,23 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
+var mnemonic = "load secret canvas siren recall mimic lizard chaos romance opera spray urban";
+
+
 module.exports = {
   networks: {
     development: {
       host: "localhost",
       port: 8545,
-      network_id: "*" // Match any network id
+      network_id: 3
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+      network_id: 3
+    },
+    main: {
+      host: "localhost",
+      port: 8545,
+      network_id: 1
     }
   }
 };
